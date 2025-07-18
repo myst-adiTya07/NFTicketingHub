1afd1835af209fa19eb06b4b10811d93e40d4764
myst-adiTya07 <106374629+myst-adiTya07@users.noreply.github.com>
2025-07-18 15:57:24 +0530

    Update NFTicketingHub.sol

--- contracts/NFTicketingHub.sol
+++ contracts/NFTicketingHub.sol
@@ -6,8 +6,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFTicketingHub is ERC721, Ownable {
     uint256 public totalEvents;
-    uint256 public ticketPrice = 0.01 ether;
-    
+    uint256 public constant TICKET_PRICE = 0.01 ether;
+
     struct Event {
         string name;
         uint256 date;
@@ -15,43 +15,56 @@ contract NFTicketingHub is ERC721, Ownable {
         uint256 ticketsSold;
         bool isActive;
     }
-    
+
     mapping(uint256 => Event) public events;
     mapping(uint256 => mapping(address => bool)) public hasPurchased;
 
     constructor() ERC721("NFTicketingHub", "NFTICKET") Ownable(msg.sender) {}
 
+    /// @notice Creates a new event
     function createEvent(
-        string memory _name,
-        uint256 _date,
-        uint256 _totalTickets
+        string calldata name_,
+        uint256 date_,
+        uint256 totalTickets_
     ) external onlyOwner {
         events[totalEvents] = Event({
-            name: _name,
-            date: _date,
-            totalTickets: _totalTickets,
+            name: name_,
+            date: date_,
+            totalTickets: totalTickets_,
             ticketsSold: 0,
             isActive: true
         });
         totalEvents++;
     }
 
+    /// @notice Allows user to purchase a ticket for a specific event
     function purchaseTicket(uint256 eventId) external payable {
-        require(events[eventId].isActive, "Event not active");
-        require(msg.value >= ticketPrice, "Insufficient payment");
-        require(!hasPurchased[eventId][msg.sender], "Already purchased");
-        require(events[eventId].ticketsSold < events[eventId].totalTickets, "Sold out");
+        Event storage evt = events[eventId];
 
-        events[eventId].ticketsSold++;
+        require(evt.isActive, "Event is not active");
+        require(msg.value >= TICKET_PRICE, "Ticket price not met");
+        require(!hasPurchased[eventId][msg.sender], "Ticket already purchased");
+        require(evt.ticketsSold < evt.totalTickets, "Tickets sold out");
+
+        evt.ticketsSold++;
         hasPurchased[eventId][msg.sender] = true;
-        _mint(msg.sender, eventId * 1000 + events[eventId].ticketsSold);
+
+        uint256 tokenId = _generateTokenId(eventId, evt.ticketsSold);
+        _mint(msg.sender, tokenId);
     }
 
+    /// @notice Toggles the active status of an event
     function toggleEventStatus(uint256 eventId) external onlyOwner {
         events[eventId].isActive = !events[eventId].isActive;
     }
 
+    /// @notice Withdraws all funds to the owner
     function withdrawFunds() external onlyOwner {
         payable(owner()).transfer(address(this).balance);
+    }
+
+    /// @dev Generates a unique tokenId based on eventId and ticket number
+    function _generateTokenId(uint256 eventId, uint256 ticketNumber) private pure returns (uint256) {
+        return eventId * 1000 + ticketNumber;
     }
 }
